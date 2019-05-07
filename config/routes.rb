1ef858935c92317ea425aa57758825638d1ad287
actiknow <deepak@resultier.com>
2019-05-07 18:03:40 +0530

    added authentication machanism

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root 'employees#index'
+  resources :employees
+  # get 'employees/new'
+  # get 'employees/create'
+  # get 'employees/update'
+  # get 'employees/edit'
+  # get 'employees/destroy'
+  # get 'employees/index'
+  # get 'employees/show'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
